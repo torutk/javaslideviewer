766dadb9829e7d813f7c90d0c76ab52b8aaf5914
TAKAHASHI,Toru <torutk@gmail.com>
2017-05-06 21:30:55 +0900

    JJUG CCC 2017 Spring m6発表スライド（下書き）

---
+++ src/javaslideviewer/Ccc008Controller.java
@@ -0,0 +1,70 @@
+/*
+ * © 2017 TAKAHASHI,Toru
+ */
+package javaslideviewer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author toru
+ */
+public class Ccc008Controller implements Initializable {
+
+    @FXML
+    public void showDefaultStage(ActionEvent event) {
+        Parent root = createSceneGraph();
+        Stage slideStage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 640, 480);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("デフォルト設定のStage");
+        stage.setOnCloseRequest(e -> {
+            stage.close();
+            e.consume();
+        });
+        
+        stage.show();
+    }
+    
+    @FXML
+    public void showTransparentStage(ActionEvent event) {
+        Parent root = createSceneGraph();
+        Scene scene = new Scene(root, 640, 480);
+        scene.setFill(Color.TRANSPARENT);
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.setTitle("デフォルト設定のStage");
+        stage.setOnCloseRequest(e -> {
+            stage.close();
+            e.consume();
+        });
+        stage.show();
+    }
+    
+    private Parent createSceneGraph() {
+        Group root = new Group();
+        Circle circle = new Circle(320, 240, 240, Color.SKYBLUE);
+        root.getChildren().add(circle);
+        return root;
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
